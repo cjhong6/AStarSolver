a2f0c23b69b598589290c648348cf6758094309a
cjhong <cjhong6@gmail.com>
2020-08-14 00:46:23 -0700

    first commit

---
+++ bearmaps/hw4/AStarSolver.java
@@ -0,0 +1,10 @@
+package bearmaps.hw4;
+
+public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
+    public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout)
+    public SolverOutcome outcome()
+    public List<Vertex> solution()
+    public double solutionWeight()
+    public int numStatesExplored()
+    public double explorationTime()
+}
